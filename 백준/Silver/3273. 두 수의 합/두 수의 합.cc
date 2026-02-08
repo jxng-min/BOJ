a4ec8a3ddf97ab5c0a8f89a4cc4e87ff02378fa9
Jongmin Kim <vwmartin@naver.com>
2026-02-08 16:44:35 +0900

    [Silver III] Title: 두 수의 합, Time: 36 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(void)
+{
+    int n = 0;
+    cin >> n;
+
+    vector<int> vec(n);
+    for(int rep = 0; rep < n; rep++)
+        cin >> vec[rep];
+
+    sort(vec.begin(), vec.end());
+
+    int x = 0;
+    cin >> x;
+
+    int s_idx = 0;
+    int e_idx = static_cast<int>(vec.size() - 1);
+
+    int answer = 0;
+    while(s_idx < e_idx)
+    {
+        int ai = vec[s_idx];
+        int aj = vec[e_idx];
+
+        if(ai + aj == x)
+        {
+            answer++;
+            e_idx--;
+        }
+        else if(ai + aj > x)
+        {
+            e_idx--;
+        }
+        else
+        {
+            s_idx++;
+        }
+    }
+
+    cout << answer;
+}
